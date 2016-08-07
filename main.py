6fffa2d4d6e919bdc1345362f0495d0f6dc8d6d0
anatoly <anatoly.kussul@gmail.com>
2016-08-07 20:54:23 +0300

    added data persistence

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import asyncio
 import logging
 import argparse
+import shelve
+import glob
+import os
 
 from aiohttp import web
 import aiohttp_jinja2
@@ -19,6 +22,7 @@ def parse_args():
     arg_parser.add_argument('-v', '--verbose', action='store_true', help='enable debug logging')
     arg_parser.add_argument('-s', '--silent', action='store_true', help='show only critical logging')
     arg_parser.add_argument('--drop', action='store_true', help='drop tables before start')
+    arg_parser.add_argument('--clean', action='store_true', help='clean up data persistence')
     args = arg_parser.parse_args()
 
     if args.verbose and args.silent:
@@ -48,15 +52,52 @@ def init_app(db, loop=None):
     return app
 
 
+def clean_up_shelve():
+    logging.info('Cleaning up shelve...')
+    for f in glob.glob(settings.SHELVE_FILENAME + '*'):
+        os.remove(f)
+
+
+def load_from_shelve(app, shelf):
+    app_keys = ['shift', 'visitors', 'cash', 'shift', 'user_id', 'username', 'is_admin']
+    any_loaded = False
+    for key in app_keys:
+        if key in shelf:
+            any_loaded = True
+            app[key] = shelf[key]
+    if any_loaded:
+        logging.info('Loaded system state from file.')
+
+
+def save_to_shelve(app, shelf):
+    app_keys = ['shift', 'visitors', 'cash', 'shift', 'user_id', 'username', 'is_admin']
+    for key in app_keys:
+        if key in app:
+            shelf[key] = app[key]
+    logging.info('Saved current system state.')
+
+
+def init_shelve(clean=False):
+    if clean:
+        clean_up_shelve()
+    shelf = shelve.open(settings.SHELVE_FILENAME)
+    return shelf
+
+
 def main():
     loop = asyncio.get_event_loop()
     args = parse_args()
     setup_logging(verbose=args.verbose, silent=args.silent)
     db = init_db(args.drop)
     app = init_app(db, loop=loop)
+    shelf = init_shelve(args.clean)
+    load_from_shelve(app, shelf)
     logging.info('Starting app on port {}...'.format(settings.PORT))
-    web.run_app(app, port=settings.PORT, print=lambda x: x)
-    logging.info('Stopped.')
+    try:
+        web.run_app(app, port=settings.PORT, print=lambda x: x)
+    finally:
+        save_to_shelve(app, shelf)
+        logging.info('Stopped.')
 
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -3,11 +3,14 @@ PORT = 8853
 TEMPLATES_PATH = 'templates'
 STATIC_PATH = 'static'
 
+# data persistence
+SHELVE_FILENAME = 'current_state.db'
+
 # postgresql settings
 POSTGRES_CONNECTION_SETTINGS = {
-    'database': 'aact',
-    'user': 'postgres',
-    'password': 'njirf5',
+    'database': 'eblank',
+    'user': 'eblank',
+    'password': 'i_am_password',
     'host': 'localhost',
     'port': 5432,
 }
