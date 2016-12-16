75e31a0744cf7eddf6694b9ffd7438d629dd0865
Anatoly Kussul <Anatoly Kussul>
2016-12-16 23:11:53 +0200

    added chrome app run

--- eblank/main.py
+++ eblank/main.py
@@ -1,5 +1,8 @@
 import logging
 import shelve
+import psutil
+import os
+import time
 
 import aiohttp_jinja2
 import asyncio
@@ -72,6 +75,8 @@ def run_app(app, *, host='0.0.0.0', port=None,
     logging.info("Started on {scheme}://{prompt}:{port}/".format(scheme=scheme, prompt=prompt, port=port))
 
     try:
+        chrome_app = run_chrome_app()
+        asyncio.ensure_future(wait_for_process(chrome_app, loop=loop), loop=loop)
         loop.run_forever()
     except KeyboardInterrupt:  # pragma: no branch
         pass
@@ -82,6 +87,50 @@ def run_app(app, *, host='0.0.0.0', port=None,
         loop.run_until_complete(handler.finish_connections(shutdown_timeout))
         loop.run_until_complete(app.cleanup())
     loop.close()
+
+
+async def wait_for_process(process, loop=None):
+    while process.is_running():
+        await asyncio.sleep(0.1, loop=loop)
+    logging.info('Eblank process stopped')
+    loop.stop()
+
+
+def get_chrome_processess():
+    chrome_pids = set()
+    for proc in psutil.process_iter():
+        try:
+            if proc.name() == "chrome.exe":
+                chrome_pids.add(proc.pid)
+        except psutil.AccessDenied:
+            logging.warning("Permission error or access denied on process")
+
+    return chrome_pids
+
+
+def run_chrome_app():
+    chromes_before = get_chrome_processess()
+    os.system('start chrome --app=\"http://localhost:8853\"')
+    eblank_process = None
+    total_sleep_time = 0
+    logging.info('Trying to find chrome process')
+    while not eblank_process:
+        time.sleep(0.1)
+        total_sleep_time += .1
+        chromes_after = get_chrome_processess()
+        unique_pids = chromes_after - chromes_before
+        if not unique_pids:
+            continue
+        if len(unique_pids) > 1:
+            logging.critical('Found more than one started chrome processes')
+            return
+        eblank_pid = next(iter(unique_pids))
+        try:
+            eblank_process = psutil.Process(eblank_pid)
+        except psutil.NoSuchProcess:
+            logging.warning('no such process')
+    logging.info('Found Eblank process in {} seconds (PID: {})'.format(total_sleep_time, eblank_pid))
+    return eblank_process
 
 
 def main():
